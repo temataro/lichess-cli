0317a61fcf9c003ce8bc1f794079fe4ebd93fef7
Temesgen Ataro <atarotemesgen@gmail.com>
2025-06-05 00:54:01 +0400

    Working broadcast stream call

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 
+import json
 import requests
+
+
+pgn = (
+    "GdK93YSo"  # https://lichess.org/api/stream/broadcast/round/{broadcastRoundId}.pgn"
+)
 
 
 def main() -> None:
     params: dict[str, str] = {"access_token": "lip_DRS4U0s4YIsll0dMv5h2"}
-
+    hdr = {
+        "Content-type": "application/x-chess-pgn",  # This is what you probably wanted
+    }
     response = requests.get(
-        "https://lichess.org/api/broadcast",
-        headers={"Content-type": "application/json"},
+        f"https://lichess.org/api/broadcast/round/{pgn}.pgn",
+        headers=hdr,
         params=params,
     )
     response.encoding = "utf-8"
     text = response.text
 
-    print(text.split(","), end="\n")
+    # json_print = lambda response: print(json.dumps(response.json(), indent=2, ensure_ascii=False))
+    # print(response.json()) # , indent=2, ensure_ascii=False))
+    print(text)
 
 
 if __name__ == "__main__":
